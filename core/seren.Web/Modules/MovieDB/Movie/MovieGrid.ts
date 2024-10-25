fe15035aef986822bc361c5fc738029f605463fa
skputra-aol <skputra2016@aol.com>
2024-10-25 15:56:19 +0700

    customize quick search

--- core/seren.Web/Modules/MovieDB/Movie/MovieGrid.ts
+++ core/seren.Web/Modules/MovieDB/Movie/MovieGrid.ts
@@ -2,10 +2,27 @@
 import { Decorators, EntityGrid } from '@serenity-is/corelib';
 import { MovieDialog } from './MovieDialog';
 
+import { QuickSearchField } from '@serenity-is/corelib'; //(#createdropdownlist)
+import { localText } from '@serenity-is/corelib'; //(#createdropdownlist)
+
 @Decorators.registerClass('seren.MovieDB.MovieGrid')
 export class MovieGrid extends EntityGrid<MovieRow> {
     protected getColumnsKey() { return MovieColumns.columnsKey; }
     protected getDialogType() { return MovieDialog; }
     protected getRowDefinition() { return MovieRow; }
     protected getService() { return MovieService.baseUrl; }
+
+    //Begin-create dropdown list search by field name (#createdropdownlist)
+    // note : dotnet sergen t (https://serenity.is/docs/tutorials/movies/05-customizing-quick-search)
+    protected getQuickSearchFields(): QuickSearchField[] {
+        const txt = s => localText(`Db.${MovieRow.localTextPrefix}.${s}`);
+        const fld = MovieRow.Fields;
+        return [
+            { name: "", title: "All" },
+            { name: fld.Description, title: txt(fld.Description) },
+            { name: fld.Storyline, title: txt(fld.Storyline) },
+            { name: fld.Year, title: txt(fld.Year) }
+        ];
+    }
+    //End
 }
--- core/seren.Web/Modules/MovieDB/Movie/MovieRow.cs
+++ core/seren.Web/Modules/MovieDB/Movie/MovieRow.cs
@@ -1,4 +1,4 @@
-﻿using Serenity.ComponentModel;
+using Serenity.ComponentModel;
 using Serenity.Data;
 using Serenity.Data.Mapping;
 using System;
@@ -16,16 +16,18 @@ public sealed class MovieRow : Row<MovieRow.RowFields>, IIdRow, INameRow
     [DisplayName("Movie Id"), Identity, IdProperty]
     public int? MovieId { get => fields.MovieId[this]; set => fields.MovieId[this] = value; }
 
-    [DisplayName("Title"), Size(200), NotNull, QuickSearch, NameProperty]
+    //[DisplayName("Title"), Size(200), NotNull, NameProperty, QuickSearch]
+    [DisplayName("Title"), Size(200), NotNull, NameProperty, QuickSearch(SearchType.StartsWith)]  // start with string
+    
     public string Title { get => fields.Title[this]; set => fields.Title[this] = value; }
 
-    [DisplayName("Description"), Size(1000)]
+    [DisplayName("Description"), Size(1000), QuickSearch]
     public string Description { get => fields.Description[this]; set => fields.Description[this] = value; }
 
-    [DisplayName("Storyline")]
+    [DisplayName("Storyline"), QuickSearch]
     public string Storyline { get => fields.Storyline[this]; set => fields.Storyline[this] = value; }
-
-    [DisplayName("Year")]
+    //[DisplayName("Year")]
+    [DisplayName("Year"), QuickSearch(SearchType.Equals, numericOnly: 1)]  //only match 4 digit year
     public int? Year { get => fields.Year[this]; set => fields.Year[this] = value; }
 
     [DisplayName("Release Date")]
@@ -33,6 +35,9 @@ public sealed class MovieRow : Row<MovieRow.RowFields>, IIdRow, INameRow
 
     [DisplayName("Runtime (Mins)")]
     public int? Runtime { get => fields.Runtime[this]; set => fields.Runtime[this] = value; }
+
+    //[DisplayName("Kind"), NotNull]  // tambahkan enum kind
+    //public MovieKind? Kind { get => fields.Kind[this]; set => fields.Kind[this] = value; }
 
     public class RowFields : RowFieldsBase
     {
@@ -43,6 +48,8 @@ public sealed class MovieRow : Row<MovieRow.RowFields>, IIdRow, INameRow
         public Int32Field Year;
         public DateTimeField ReleaseDate;
         public Int32Field Runtime;
+
+        //public EnumField<MovieKind> Kind;  // tambahkan enum kind
 
     }
 }
